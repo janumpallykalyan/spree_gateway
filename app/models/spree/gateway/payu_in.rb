e6119548b4ff055fd429143ee3a03b82a72c79eb
Kalyan Janumpally <janumpallykalyan@gmail.com>
2015-12-06 15:35:54 +0530

    salt key

--- app/models/spree/gateway/payu_in.rb
+++ app/models/spree/gateway/payu_in.rb
@@ -2,7 +2,8 @@ module Spree
   class Gateway::PayuIn < Gateway
     preference :login, :string
     preference :password, :string
-
+    preference :salt, :string
+    preference :key, :string
     def provider_class
       ActiveMerchant::Billing::PayuInGateway
     end
