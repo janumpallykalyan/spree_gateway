c114f1379fb38c0bd8c6175b4e589db7635a1666
Kalyan Janumpally <janumpallykalyan@gmail.com>
2015-12-06 13:18:08 +0530

    chnage

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -31,7 +31,7 @@ module SpreeGateway
       app.config.spree.payment_methods << Spree::Gateway::StripeGateway
       app.config.spree.payment_methods << Spree::Gateway::UsaEpayTransaction
       app.config.spree.payment_methods << Spree::Gateway::Worldpay
-      app.config.spree.payment_methods << Spree::Gateway::PayUbiz
+      app.config.spree.payment_methods << Spree::Gateway::PayuIn
     end
 
     def self.activate
